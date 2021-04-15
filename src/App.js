1888d7011185edc7df93b667937c9fd779c07cec
suhel311 <76218258+suhel311@users.noreply.github.com>
2021-04-16 00:35:39 +0600

    After-jeson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Gallery from "./Gallery";
+import Navbar from "./Navbar";
+import { Redirect, Route, Switch } from "react-router";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/gallery" component={Gallery} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
+
+
